36c1ce85597ff7132e9914dda31fca57e23d1e70
Selim Reza <selim.reza.dia@gmail.com>
2024-05-21 19:46:40 +0600

    homepage degin completed except responsiveness

--- assets/js/accrodion.js
+++ assets/js/accrodion.js
@@ -5,22 +5,28 @@ let faq__title = document.querySelectorAll(".faq__title");
 for (let i = 0; i < faq__title.length; i++) {
     faq__title[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        this.children[0].classList.add("display-none");
-        this.children[1].classList.remove("display-none");
-        for (let j = 0; j < faq__title.length; j++) {
-            if (faq__title[j].classList.contains("active")) {
-                continue;
-            } else {
-                faq__title[j].children[0].classList.remove("display-none");
-                faq__title[j].children[1].classList.add("display-none");
-            }
-        }
-        this.parentElement.classList.toggle("faq__border");
+        let down = 0;
+        let right = 270;
+        this.children[0].style.transform = `rotate(${right}deg)`;
+
+        this.parentElement.classList.add("faq__border");
         let panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
             panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+
+        //removing classes and styles where active is not present
+        for (let j = 0; j < faq__title.length; j++) {
+            if (faq__title[j].classList.contains("active") && this === faq__title[j]) {
+                continue;
+            } else {
+                faq__title[j].parentElement.classList.remove("faq__border");
+                faq__title[j].children[0].style.transform = `rotate(${down}deg)`;
+                faq__title[j].nextElementSibling.style.maxHeight = null;
+                faq__title[j].classList.remove("active");
+            }
         }
     })
 }
