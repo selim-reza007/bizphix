6d85ba86ba29f6403717df9535f9bc2ff6cfa847
Selim Reza <selim.reza.dia@gmail.com>
2024-05-27 21:23:57 +0600

    Header's issues are solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,35 +8,42 @@ playBtn.addEventListener("click", () => {
     document.querySelector(".hero__video").classList.remove("display-none");
 })
 
-let mobileMenu = document.querySelector(".header__menu-m"); //not needed
-let mobileMenuOpen = document.querySelector(".header__menu-open-m"); //*
-let mobileMenuClose = document.querySelector(".header__mobile-menu-close"); //*
+let mobileMenu = document.querySelector(".header__navigation");
+let mobileMenuOpen = document.querySelector(".header__menu-open"); //*
+let mobileMenuClose = document.querySelector(".header__menu-close"); //*
 let dropdownMobile = document.querySelector(".header__dropdown-m");
-let dropdownopen = document.querySelector(".dropdown-open-close");
+let dropdownopen = document.querySelector(".header__li-dropdown");
+let dropdownToggleIcon = document.querySelector(".header__dropdown-icon");
 
 mobileMenuOpen.addEventListener("click", function () {
+    mobileMenu.style.display = "block";
     this.classList.add("display-nothing");
     mobileMenuClose.classList.remove("display-nothing");
 });
 
 mobileMenuClose.addEventListener("click", function () {
+    mobileMenu.style.display = "none";
     this.classList.add("display-nothing");
     mobileMenuOpen.classList.remove("display-nothing");
+});
+
+dropdownToggleIcon.addEventListener("click", function () {
+    dropdownopen.classList.toggle("display-nothing");
 })
 
-$(document).ready(function () {
-    var menu = $('.header');
-    var origOffsetY = menu.offset().top;
+// $(document).ready(function () {
+//     var menu = $('.header');
+//     var origOffsetY = menu.offset().top;
 
-    function onScroll() {
-        if ($(window).scrollTop() >= origOffsetY) {
-            menu.addClass('sticky');
-        } else {
-            menu.removeClass('sticky');
-        }
-        console.log("On Scroll");
-    }
+//     function onScroll() {
+//         if ($(window).scrollTop() >= origOffsetY) {
+//             menu.addClass('sticky');
+//         } else {
+//             menu.removeClass('sticky');
+//         }
+//         console.log("On Scroll");
+//     }
 
-    document.onscroll = onScroll;
+//     document.onscroll = onScroll;
 
-});
+// });
